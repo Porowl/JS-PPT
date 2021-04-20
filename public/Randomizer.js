ae2e445caf8c01b9cfd9f2eaa5ba62f8a45d0a18
YoungChan Song <youngchan0518@gmail.com>
2021-04-20 14:07:30 +0000

    init bag for puyo adjusted to max at 3 colors

--- public/Randomizer.js
+++ public/Randomizer.js
@@ -9,8 +9,7 @@ export default class Randomizer{
 		this.initPieces();
     }
 
-    getPiece = (index) =>
-    {
+    getPiece = (index) => {
         while(this.pieces.length<index+8) this.addPiece();
         return this.pieces[index];
     }
@@ -41,29 +40,47 @@ export default class Randomizer{
 
     refillBag = () => {if(!this.bag.length) for(let i = 0; i<7; i++)this.bag.push(i);}
 	
-	getBubbling = (index = 0) =>
-    {
+	getBubbling = (index = 0) => {
 		index = index % this.bubblings.length;
         return this.bubblings[index];
     }
 	
-	getNextBubblings = index =>
-	{
+	getNextBubblings = index => {
 		this.bubblings.slice(index,index+3);
 	}
 	
 	initBubblingBag = () => {
 		let arr = [];
-		
 		for(let i = 0; i<16; i++) {	
 			for(let j = 0; j<16; j++) {
 				arr.push(j);	
 			}
-			while(arr.length>0){
+			while(arr.length){
 				let rand = this.engine()*arr.length|0;
 				let pulled = arr.splice(rand,1);
-				this.bubblings.push(pulled);				
+				this.bubblings.push(pulled);	
 			}
 		}
+		while(this.checkIfIllegalStart()) this.shuffleInitBag();
+	}
+	
+	checkIfIllegalStart = () => {
+		let p0 = this.bubblings[0];
+		let p1 = this.bubblings[1];
+		const c0 = ( p0 & 0xc ) / 0x4;
+		const c1 = p0 % 0x4;
+		const c2 = ( p1 & 0xc ) / 0x4;
+		const c3 = p1 % 0x4;
+		let result = 0b0000;
+		
+		result = result | (0b0001 << c0) | (0b0001 << c1) | (0b0001 << c2) | (0b0001 << c3);
+		return result == 0b1111;
+	}
+	
+	shuffleInitBag = () => {
+		let rand = 2 + (this.engine() * 14 | 0);
+		let temp = this.bubblings[rand];
+		this.bubblings[rand] = this.bubblings[0];
+		this.bubblings[0] = temp;
 	}
 }
--- src/LobbyManager.js
+++ src/LobbyManager.js
@@ -4,33 +4,46 @@ export default class LobbyManager {
 	constructor(roomManager) {
 		this.roomManager = roomManager;
 		this.rooms = [];
+		this.sockets = [];
 	}
 
-	createRoom = (socket, type, roomid) => {
-		if(!roomid) roomid = socket.id;
+	createRoom = (socket, type, roomid, password) => {
+		if(!roomid) roomid = socket.id; 
 		console.log(`${socket.id} has made room: ${roomid}`);
-		this.rooms[roomid] = new customRoom(socket, roomid);
+		this.rooms[roomid] = new customRoom(socket, roomid, password);
+		this.sockets[socket.id] = roomid;
 		io.to(socket.id).emit('create',type);
-	};
-
-	getRoomArr = () => {
-		
 	}
 
-	askPassword = () => {
-		
-	}
-	
+	getRoomArr = () =>  Object.keys(this.rooms);
+
 	enterRoom = (socket, roomid) => {
+		let room = this.rooms[roomid];
+		if(room){
+			if(room.requirePassword()){
+				socket.emit('askPassword',roomid)
+			} else {
+				//let user enter
+			}
+		} else {
+			//alert user the room seems doesn't exist anymore!
+		}
 	}
 	
-	leave = () => {
+	destroyRoom = () => {
 		
 	};
 }
 
 class customRoom{
-	constructor(){
-		
+	constructor(socket, roomid, password){
+		this.socket = socket;
+		this.roomid = roomid;
+		this.password = password;
+		this.passwordRequired = (password)?true:false;
 	}
+	
+	matchPassword = (password) => password === this.password;
+	requiresPassword = () => this.passwordRequired;
+	getRoomOwner = () => {};
 }
